9799046efde5b6726568fa99ffd4dbedf342adef
Rufatello <142746692+Rufatello@users.noreply.github.com>
2023-10-19 10:30:44 +0300

    Update main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,10 +3,16 @@ import psycopg2
 import csv
 
 def load_file(path_file):
+    
+    '''функция для чтения файла, ему присваиваем переменные, которые написанные ниже'''
+    
     with open(path_file, 'r') as file:
         reader = csv.DictReader(file)
         data = list(reader)
         return data
+        
+'''пути для передачи значения в функцию load_file'''
+
 employees_data = load_file('/home/geydarovr/Загрузки/pythonProject/postgres-homeworks/homework-1/north_data/employees_data.csv')
 orders_data = load_file('/home/geydarovr/Загрузки/pythonProject/postgres-homeworks/homework-1/north_data/orders_data.csv')
 customers_data = load_file('/home/geydarovr/Загрузки/pythonProject/postgres-homeworks/homework-1/north_data/customers_data.csv')
@@ -14,12 +20,16 @@ conn = psycopg2.connect(host='localhost', database='north', user='postgres', pas
 try:
     with conn:
         with conn.cursor() as cur:
+            '''создаем 3 цикла для заполнения таблиц'''
             for cus in customers_data:
                 cur.execute('INSERT INTO customers VALUES (%s,%s,%s) ON CONFLICT (customer_id) DO NOTHING',(cus['customer_id'], cus['company_name'], cus['contact_name']))
                 conn.commit
 
             cur.execute("SELECT * FROM customers")
             rows = cur.fetchall()
+            
+            '''вывод таблицы'''
+            
             for row in rows:
                 print(row)
 
@@ -28,6 +38,9 @@ try:
                 conn.commit
             cur.execute('SELECT*FROM orders')
             rows = cur.fetchall()
+            
+            '''вывод таблицы'''
+            
             for row in rows:
                 print(row)
 
@@ -36,8 +49,49 @@ try:
                 conn.commit
             cur.execute("SELECT * FROM employees")
             rows = cur.fetchall()
+            
+            '''вывод таблицы'''
+            
             for row in rows:
                 print(row)
 finally:
     conn.close()
+    #Второй вариант закоментирован
 
+"""Скрипт для заполнения данными таблиц в БД Postgres."""
+# import psycopg2
+# import csv
+# 
+# def load_file(path_file):
+#     with open(path_file, 'r') as file:
+#         reader = csv.DictReader(file)
+#         data = list(reader)
+#         return data
+# employees_data = load_file('/home/geydarovr/Загрузки/pythonProject/postgres-homeworks/homework-1/north_data/employees_data.csv')
+# orders_data = load_file('/home/geydarovr/Загрузки/pythonProject/postgres-homeworks/homework-1/north_data/orders_data.csv')
+# customers_data = load_file('/home/geydarovr/Загрузки/pythonProject/postgres-homeworks/homework-1/north_data/customers_data.csv')
+# conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
+# try:
+#     with conn:
+#         with conn.cursor() as cur:
+#             for cus, emp, ort in zip(customers_data, employees_data, orders_data):
+#                 cur.execute('INSERT INTO customers VALUES (%s,%s,%s) ON CONFLICT (customer_id) DO NOTHING',(cus['customer_id'], cus['company_name'], cus['contact_name']))
+#                 cur.execute('INSERT INTO employees VALUES (%s,%s,%s,%s,%s,%s) ON CONFLICT (employee_id) DO NOTHING', (emp['employee_id'], emp['first_name'], emp['last_name'], emp["title"], emp["birth_date"], emp['notes']))
+#                 cur.execute('INSERT INTO orders VALUES (%s,%s,%s,%s,%s) ON CONFLICT (order_id) DO NOTHING', (ort['order_id'], ort['customer_id'], ort['employee_id'], ort['order_date'], ort['ship_city']))
+#                 conn.commit
+# 
+#             cur.execute("SELECT * FROM customers")
+#             rows = cur.fetchall()
+#             cur.execute('SELECT*FROM orders')
+#             rows_1 = cur.fetchall()
+#             cur.execute("SELECT * FROM employees")
+#             rows_2 = cur.fetchall()
+#             for row in rows:
+#                 print(row)
+#             for row_1 in rows_1:
+#                 print(row_1)
+#             for row_2 in rows_2:
+#                 print(row_2)
+# 
+# finally:
+#     conn.close()
