c421b5c9f3ab0668cdca8b5ae15f0b1b9f744bba
Rufatello <142746692+Rufatello@users.noreply.github.com>
2023-10-19 09:59:57 +0300

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,4 +1,3 @@
--- SQL-команды для создания таблиц
 CREATE TABLE employees (
 	employee_id int PRIMARY KEY,
 	first_name varchar(50),
@@ -15,6 +14,7 @@ CREATE TABLE customers(
 CREATE TABLE orders(
 	order_id int PRIMARY KEY,
 	customer_id varchar(6) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
 	order_date date,
 	ship_city varchar(50)
 );
